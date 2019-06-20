22cf36a22085c5f737ac873055b7571db1e066f0
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-06-19 21:21:15 -0400

    added routes and variables to store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import searchTrack from './api_services/searchTrack.js'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    track: '',
+    error_message: '',
   },
+
   mutations: {
+    setTrack(state, payload){
+      state.track = payload
+    },
 
+    setErrorMessage(state, payload){
+      state.error_message = payload
+    },
   },
-  actions: {
 
+  actions: {
+    //get track info from api or set track to null
+    //if an error ocurres
+    getTrack(context, payload){
+      searchTrack(payload).then(res => {
+        context.commit('setTrack', res.data)
+        console.log(res.data)
+
+      }).catch(res => {
+        console.log(res)
+
+        context.commit('setTrack', null)
+        context.commit('setErrorMessage', 'sorry an error ocurred with the backend')
+
+      })
+    }
   }
 })
