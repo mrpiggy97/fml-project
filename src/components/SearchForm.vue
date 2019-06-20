17007c7da2839de87c113b335d980c2821e45ba7
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-06-19 21:23:10 -0400

    created searchForm component

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,80 @@
+<template>
+    <!--component is designed to work its layout with css grid-->
+    <!--it will ocupy the area at which it is being rendered-->
+    <!--on mobile but only the entire length in 3/4 of the width area on desktop-->
+    <div class="searchform-component">
+        <form class="search-form" @submit="sendQuery">
+            <input type="text" class="search-input" v-model="query"/>
+            <button type="submit" class="search-button">
+                <i class="fa fa-search"></i>
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+import trackBus from '../event-buses/trackBus.js'
+
+export default {
+    name: "SearchForm",
+
+    data(){
+        return{
+            //query will work with an event bus to pass data from MainHeader
+            //component and Tracks view to Tracks view itself to make a call api
+            query: '',
+        }
+    },
+
+    methods:{
+        sendQuery(e){
+            e.preventDefault()
+            trackBus.$emit('setQuery', this.query)
+        }
+    }
+}
+</script>
+
+<style>
+div.searchform-component{
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+}
+
+form.search-form{
+    display: grid;
+    grid-template-columns: 5fr 100px;
+    grid-template-rows: 40px;
+    grid-template-areas: "search-input search-button";
+    grid-gap: 5px;
+    align-content: center;
+}
+
+input[type="text"].search-input{
+    grid-area: search-input;
+    border: teal solid;
+    border-radius: 5px;
+    border-top-right-radius: 0%;
+    border-bottom-right-radius: 0%;
+    font-size: large;
+}
+
+button.search-button{
+    grid-area: search-button;
+    color: white;
+    background-color: teal;
+    border: transparent solid;
+    border-radius: 5px;
+    border-top-left-radius: 0%;
+    border-bottom-left-radius: 0%;
+}
+
+@media screen and (max-width: 769px){
+    form.search-form{
+        grid-template-columns: 5fr 1fr;
+        grid-template-rows: 1fr;
+        grid-gap: 2px;
+    }
+}
+</style>
