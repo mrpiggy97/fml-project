e321115f0a0343067b18d063a6ef01f71c643932
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-06-21 23:52:23 -0400

    eliminated error_message alongside setter

--- src/store.js
+++ src/store.js
@@ -8,16 +8,11 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     track: '',
-    error_message: '',
   },
 
   mutations: {
     setTrack(state, payload){
       state.track = payload
-    },
-
-    setErrorMessage(state, payload){
-      state.error_message = payload
     },
   },
 
@@ -27,13 +22,10 @@ export default new Vuex.Store({
     getTrack(context, payload){
       searchTrack(payload).then(res => {
         context.commit('setTrack', res.data)
-        console.log(res.data)
 
       }).catch(res => {
-        console.log(res)
 
         context.commit('setTrack', null)
-        context.commit('setErrorMessage', 'sorry an error ocurred with the backend')
 
       })
     }
