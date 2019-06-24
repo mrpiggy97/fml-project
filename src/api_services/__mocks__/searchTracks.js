819559f28736075694bb6f1ec6fdd484f4d917e2
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-06-24 01:18:02 -0400

    mock for searchTracks.js

---
+++ src/api_services/__mocks__/searchTracks.js
@@ -0,0 +1,18 @@
+
+function searchTracks(q){
+    return Promise.resolve({
+        data:{
+            tracks:{
+                items:[{
+                    name: q,
+                    album: null,
+                    artists: [{name: 'shakira'}],
+                    preview_url: null,
+                    id: "lkd234343kjl;jls"
+                }]
+            }
+        }
+    })
+}
+
+export default searchTracks
