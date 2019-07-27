7b53849b3158c7ea475573878d93d0ff10b82d17
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-07-27 00:51:03 -0400

    re-orginized Tracks view logic

--- src/views/Tracks.vue
+++ src/views/Tracks.vue
@@ -16,7 +16,7 @@
 
         <div class="message">
             <transition name="slide">
-                <span class="message-text" v-show="message">{{message}}</span>
+                <span class="message-text" v-show="tracks.loading === false">{{message}}</span>
             </transition>
         </div>
 
@@ -28,65 +28,114 @@
 </template>
 
 <script>
-import { onCreated, onMounted, watch } from 'vue-function-api'
+import { value, onCreated, onMounted, watch, computed } from 'vue-function-api'
 import { setTimeout } from 'timers'
-
-import setApp from './functions/Tracks'
 import searchTracks from '@/api_services/searchTracks.js'
 
 import TrackInfo from '@/components/TrackInfo.vue'
 import SearchForm from '@/components/SearchForm.vue'
+
+
+function APICall(){
+
+    const tracks = value({items: [], loading: true, showTrack: false})
+    const firstLoad = value(true)
+    const backendError = value(false)
+    const message = computed(() => {
+        
+        let numberOfTracks = null
+
+        if(tracks.value.items.length > 0 && backendError.value === false){
+            numberOfTracks = `we found ${tracks.value.items.length} tracks`
+        }
+        else if(tracks.value.items.length == 0 && backendError.value === true){
+            numberOfTracks = 'sorry there was an error with the api'
+        }
+
+        return numberOfTracks
+    })
+
+    const searching = () => {
+        if(backendError.value === true){
+            backendError.value = false
+        }
+        tracks.value.loading = true
+        tracks.value.showTrack = false
+    }
+
+    const searchDone = () => {
+        tracks.value.loading = false
+        tracks.value.showTrack = true
+    }
+
+    const getTracks = async (querySearch) => {
+        
+        if(firstLoad.value === false){
+            searching()
+        }
+
+        try{
+            let response = await searchTracks(querySearch)
+            tracks.value.items = response.data.tracks.items
+        }
+
+        catch(error){
+            backendError.value = true
+        }
+
+        setTimeout(() => {
+            searchDone()
+        }, 1000)
+    }
+
+    return{
+        tracks,
+        firstLoad,
+        message,
+        getTracks
+    }
+}
+
+function mobileLayout(){
+
+    const isMobile = value(window.screen.width <= 769)
+
+    const onResize = () => {
+        isMobile.value = window.screen.width <= 769
+    }
+
+    return{
+        isMobile,
+        onResize
+    }
+}
+
+function getStore(context){
+    
+    const query = computed(() => {
+        return context.root.$store.state.query
+    })
+
+    const getTrack = (id) => {
+        context.root.$store.dispatch('getTrack', id)
+    }
+
+    return{
+        query,
+        getTrack
+    }
+}
+
 
 export default {
     name: 'Tracks',
 
     setup(props, context){
 
-        //state and computed propertries
-        const { tracks, backendError, message, isMobile, query, firstLoad } = setApp(context)
-
-        //methods
-        function getTrack(id){
-            context.root.$store.dispatch('getTrack', id)
-        }
-
-        function searching(){
-            if(backendError.value === true){
-                backendError.value = false
-            }
-
-            tracks.value.loading = true,
-            tracks.value.showTrack = false
-        }
-
-        function searchDone(){
-            tracks.value.loading = false
-            tracks.value.showTrack = true
-        }
-
-        async function getTracks(querySearch){
-            
-            if(firstLoad.value === false){
-                searching()
-            }
-
-            try{
-                let response = await searchTracks(querySearch)
-                tracks.value.items = response.data.tracks.items
-            }
-
-            catch(error){
-                backendError.value = true
-            }
-
-            setTimeout(() => {
-                searchDone()
-            }, 1000)
-        }
-
-        function onResize(){
-            isMobile.value = window.screen.width <= 769
-        }
+        //state and computed
+        const { tracks, firstLoad, message, getTracks } = APICall()
+        const { isMobile, onResize } = mobileLayout()
+        const { query, getTrack } = getStore(context)
 
         watch(() => query.value, (newVal) => {
             getTracks(newVal)
